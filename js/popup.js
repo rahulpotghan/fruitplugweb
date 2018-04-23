76fcd5d41f97c5acc612f1397dfa5e5dcca07d1e
Rahul <rahul.potghan@gmail.com>
2018-04-22 22:31:51 -0700

    Animations changes
    Pranav’s changes to fruitplug

---
+++ js/popup.js
@@ -0,0 +1,213 @@
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: 'https://vimeo.com/123123',
+        type: 'iframe' // this overrides default type
+      },
+      {
+        src: $('<div class="white-popup">Dynamically created element</div>'), // Dynamically created element
+        type: 'inline'
+      },
+      {
+        src: '<div class="white-popup">Popup from HTML string</div>', // HTML string
+        type: 'inline'
+      },
+      {
+        src: '#my-popup', // CSS selector of an element on page that should be used as a popup
+        type: 'inline'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+$('#open-cdpopup').magnificPopup({
+    items: [
+      {
+        src: '../images/portfolio/characterdesign/1.jpg',
+        title: '1'
+      },
+      {
+        src: '../images/portfolio/characterdesign/2.jpg',
+        title: '2'
+      },
+      {
+        src: '../images/portfolio/characterdesign/3.jpg',
+        title: '3'
+      },
+        {
+        src: '../images/portfolio/characterdesign/4.jpg',
+        title: '4'
+      },
+        {
+        src: '../images/portfolio/characterdesign/5.jpg',
+        title: '5'
+      },
+        {
+        src: '../images/portfolio/characterdesign/6.jpg',
+        title: '6'
+      },
+        {
+        src: '../images/portfolio/characterdesign/7.jpg',
+        title: '7'
+      },
+        {
+        src: '../images/portfolio/characterdesign/8.jpg',
+        title: '8'
+      },
+        {
+        src: '../images/portfolio/characterdesign/9.jpg',
+        title: '9'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+$('#open-sbpopup').magnificPopup({
+    items: [
+      {
+        src: '../images/portfolio/storybook/1.jpg',
+        title: '1'
+      },
+      {
+        src: '../images/portfolio/storybook/2.jpg',
+        title: '2'
+      },
+      {
+        src: '../images/portfolio/storybook/3.jpg',
+        title: '3'
+      },
+        {
+        src: '../images/portfolio/storybook/4.jpg',
+        title: '4'
+      },
+        {
+        src: '../images/portfolio/storybook/5.jpg',
+        title: '5'
+      },
+        {
+        src: '../images/portfolio/storybook/6.jpg',
+        title: '6'
+      },
+        {
+        src: '../images/portfolio/storybook/7.jpg',
+        title: '7'
+      },
+        {
+        src: '../images/portfolio/storybook/8.jpg',
+        title: '8'
+      },
+        {
+        src: '../images/portfolio/storybook/9.jpg',
+        title: '9'
+      },
+        {
+        src: '../images/portfolio/storybook/10.jpg',
+        title: '10'
+      },
+        {
+        src: '../images/portfolio/storybook/11.jpg',
+        title: '11'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+
+$('#open-capopup').magnificPopup({
+    items: [
+      {
+        src: '../images/portfolio/conceptart/1.jpg',
+        title: '1'
+      },
+      {
+        src: '../images/portfolio/conceptart/2.jpg',
+        title: '2'
+      },
+      {
+        src: '../images/portfolio/conceptart/3.jpg',
+        title: '3'
+      },
+        {
+        src: '../images/portfolio/conceptart/4.jpg',
+        title: '4'
+      },
+        {
+        src: '../images/portfolio/conceptart/5.jpg',
+        title: '5'
+      },
+        
+        {
+        src: '../images/portfolio/conceptart/7.jpg',
+        title: '6'
+      },
+        {
+        src: '../images/portfolio/conceptart/8.jpg',
+        title: '7'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#open-mapopup').magnificPopup({
+    items: [
+      {
+        src: '../images/portfolio/mangaanime/1.jpg',
+        title: '1'
+      },
+      {
+        src: '../images/portfolio/mangaanime/2.jpg',
+        title: '2'
+      },
+      {
+        src: '../images/portfolio/mangaanime/3.jpg',
+        title: '3'
+      },
+        {
+        src: '../images/portfolio/mangaanime/4.jpg',
+        title: '4'
+      },
+        {
+        src: '../images/portfolio/mangaanime/5.jpg',
+        title: '5'
+      },
+        {
+        src: '../images/portfolio/mangaanime/6.jpg',
+        title: '6'
+      },
+        {
+        src: '../images/portfolio/mangaanime/7.jpg',
+        title: '7'
+      },
+        {
+        src: '../images/portfolio/mangaanime/8.jpg',
+        title: '8'
+      },
+         {
+        src: '../images/portfolio/mangaanime/9.jpg',
+        title: '9'
+      },
+         {
+        src: '../images/portfolio/mangaanime/10.jpg',
+        title: '10'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
---
+++ popup/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/dimsemenov/pen/vKrqs.
+
+ [Magnific Popup](http://dimsemenov.com/plugins/magnific-popup/) with video, image, inline html and dynamically created html.
